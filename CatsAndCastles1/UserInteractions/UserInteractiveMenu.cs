52c1c16a3a53f622e692d10f97b4476ae16500d0
Arael Seher <a.seher.85@gmail.com>
2025-04-16 23:10:11 -0700

    corrected centering formatting for interactive menu

--- CatsAndCastles1/UserInteractions/UserInteractiveMenu.cs
+++ CatsAndCastles1/UserInteractions/UserInteractiveMenu.cs
@@ -10,7 +10,7 @@ public class UserInteractiveMenu
         ConsoleKeyInfo key;
         bool isSelected = false;
         (int left, int top) = Console.GetCursorPosition();
-        string color = "--> \u001b[32m";
+        string selected = "--> \u001b[32m";
         Console.CursorVisible = false;
 
 
@@ -18,9 +18,9 @@ public class UserInteractiveMenu
         {
             Console.SetCursorPosition(left, top);
             for (int i = 0; i < options.Count; i++)
-                Console.WriteLine($"{(optionSelected == i ? color : "    ")}{options[i]}\u001b[0m");
+                Screen.Print($"{(optionSelected != i ? "   \u001b[0m" : selected )}{options[i]}\u001b[0m");
             if (leave != "")
-                Console.WriteLine($"{(optionSelected == options.Count ? color : "    ")}{leave}\u001b[0m");
+                Screen.Print($"{(optionSelected == options.Count ? selected : "   \u001b[0m")}{leave}\u001b[0m");
             
 
             key = Console.ReadKey();
--- CatsAndCastles1/obj/Debug/net9.0/CatsAndCastles1.AssemblyInfo.cs
+++ CatsAndCastles1/obj/Debug/net9.0/CatsAndCastles1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CatsAndCastles1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a5ab285d10558883a7c2272e4a03fe1d68c7502f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+327f1f12ad395a0e07e896d0453ccb027799f3ef")]
 [assembly: System.Reflection.AssemblyProductAttribute("CatsAndCastles1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CatsAndCastles1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
